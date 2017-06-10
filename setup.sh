465bddb6ae6bb3753138609e724ec0b54d61b4e3
Tcarevskii <atcarevs@amazon.com>
2017-06-10 18:46:15 +0100

    Zsh theme

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ chsh -s $ZSH_PATH
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+sed -i -e 's/robbyrussell/af-magic/g' ~/.zshrc
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
